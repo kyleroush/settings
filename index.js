1db507089838db36f2e24f1fd9006fbf0ca3abb5
Roush,Kyle <Kyle.Roush@cerner.com>
2019-10-09 11:39:52 -0500

    check

--- index.js
+++ index.js
@@ -13,7 +13,39 @@ module.exports = (robot, _, Settings = require('./lib/settings')) => {
     })
 
     if (defaultBranch && settingsModified) {
-      return Settings.sync(context.github, context.repo(), config)
+      return Settings.sync(context.github, context.repo(), config).then(() => {
+        context.github.checks.create({
+          owner: context.repo().owner,
+          repo: context.repo().repo,
+          name: "Settings Probot",
+          head_sha: "e0062f3b8d73d93c247312131a37df7809d1454a",
+          status: "completed",
+          conclusion: "success"
+        })
+      }).catch(res => {
+  
+        const summary = `
+  There was an error while updating the settings.
+  
+  <details><summary>Failed response</summary>
+  <pre>
+  ${JSON.stringify(JSON.parse(res.message), null, 2)}
+  </pre>
+  </details>
+`
+        context.github.checks.create({
+          owner: context.repo().owner,
+          repo: context.repo().repo,
+          name: "Settings Probot",
+          head_sha: payload.after,
+          status: "completed",
+          conclusion: "failure",
+          output: {
+            title: "Settings Probot",
+            summary: summary
+          }
+        })    
+      })
     }
   })
 }
